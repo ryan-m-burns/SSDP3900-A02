060a61f455a32eea372f780cc66793dd145393b0
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-04-07 13:49:40 -0700

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+  },
+  // Base path for GitHub Pages deployment
+
+  base: '/SSDP3900-A02/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
